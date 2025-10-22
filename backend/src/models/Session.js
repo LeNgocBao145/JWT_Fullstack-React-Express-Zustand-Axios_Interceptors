71a13add1858f94178d938436ebc0597da8f54b1
LeNgocBao145 <himagito@gmail.com>
2025-10-22 10:09:30 +0700

    feat: Create Schema for session to store refresh token

---
+++ backend/src/models/Session.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true
+    },
+    refreshToken: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    expiredAt: {
+        type: Date,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+//Auto delete when timeout
+sessionSchema.index({ expiredAt: 1 }, { expireAfterSeconds: 0 });
+
+export default mongoose.model("Session", sessionSchema);
