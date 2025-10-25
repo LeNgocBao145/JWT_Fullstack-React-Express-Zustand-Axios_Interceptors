0f4916f1ee4faea633b6345eaacd0ee7cff80119
LeNgocBao145 <himagito@gmail.com>
2025-10-25 16:56:25 +0700

    feat: create UI of Sign Up page

---
+++ frontend/src/components/signup-form.tsx
@@ -0,0 +1,92 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0 border-border">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <FieldGroup>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <a href="/" className="mx-auto block w-fit text-center">
+                  <img
+                    src="/logo.svg"
+                    alt="Moji Logo"
+                  />                
+                </a>
+                <h1 className="text-2xl font-bold">Create your Moji account</h1>
+                <p className="text-muted-foreground text-sm text-balance">
+                  Welcome! Please sign up to begin!
+                </p>
+              </div>
+              <Field>
+                <Field className="grid grid-cols-2 gap-4">
+                  <Field>
+                    <FieldLabel htmlFor="firstname">First Name</FieldLabel>
+                    <Input id="firstname" type="text" required />
+                  </Field>
+                  <Field>
+                    <FieldLabel htmlFor="last-name">Last Name</FieldLabel>
+                    <Input id="last-name" type="text" required />
+                  </Field>
+                </Field>
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="username">Username</FieldLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="moji123"
+                  required
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input id="password" type="password" required />
+              </Field>                
+              <Field>
+                <Button type="submit">Create Account</Button>
+              </Field>
+              <FieldDescription className="text-center">
+                Already have an account? <a href="#">Sign in</a>
+              </FieldDescription>
+            </FieldGroup>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/placeholderSignUp.png"
+              alt="Image"
+              className="absolute top-1/2 -translate-y-1/2 object-cover"
+            />
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-balance px-6 text-center *:[a]:hover:text-primary text-muted-foreground *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  )
+}
