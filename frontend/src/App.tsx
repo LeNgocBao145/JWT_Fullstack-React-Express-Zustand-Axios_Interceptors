1a0f463f07558086f0c53adce92bf102750f5598
LeNgocBao145 <himagito@gmail.com>
2025-10-25 15:18:29 +0700

    feat: Setup shadcn/ui, Tailwind CSS và theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,35 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import {Toaster} from 'sonner';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
-  )
+    <>
+    <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* public route */}
+          <Route
+            path='/signin'
+            element={<SignInPage />}
+          />
+
+          <Route
+            path='/signup'
+            element={<SignUpPage />}
+          />
+          {/* protected route */}
+          <Route
+            path='/'
+            element={<ChatAppPage />}
+          />
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
